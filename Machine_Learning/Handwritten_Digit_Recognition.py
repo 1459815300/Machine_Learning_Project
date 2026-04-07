94f4cd10e496a74205062bf1a033deb01b7bff69
Xuyu <a1459815300@gmail.com>
2026-04-07 18:29:58 +0800

    recover code,add def use_modal.

--- Machine_Learning/Handwritten_Digit_Recognition.py
+++ Machine_Learning/Handwritten_Digit_Recognition.py
@@ -53,19 +53,31 @@ def train_modal():
     y = df.iloc[:, 0]
     print(f'x的形状: {x.shape}')
     print(f'y的形状: {y.shape}')
-    x = x/255.0
     # 使用 train_test_split 将数据划分为训练集和测试集（按 8:2 比例）
     # stratify=y 保证训练集和测试集中的类别分布与原数据一致（防止类别不均衡）
     # random_state=42 固定随机种子，确保每次划分结果可复现
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=42,stratify=y)
+    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=20,stratify=y)
     estimator = KNeighborsClassifier(n_neighbors=5)
     estimator.fit(x_train, y_train)
     print(f'准确率: {estimator.score(x_test, y_test)}')
     print(f'准确率: {accuracy_score(y_test,estimator.predict(x_test))}')
     joblib.dump(estimator, './model/手写数字识别.pkl')
     print('模型保存成功！')
-# 主程序入口：调用函数显示第20个样本
+
+def use_modal():
+    img = plt.imread('./dara/demo.png')
+   # plt.imshow(img, cmap='gray')
+   # plt.axis('off')
+   # plt.show()
+    estimator = joblib.load('./model/手写数字识别.pkl')
+    print(img.shape)
+    print(img.reshape(1,-1).shape)
+    img = img.reshape(1,-1)
+    y_pred = estimator.predict(img)
+    print(f'预测解果为:{y_pred}')
+# 主程序入口
 if __name__ == '__main__':
     train_modal()
+    use_modal()
 
 
